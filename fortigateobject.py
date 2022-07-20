8a851155ebacf92918c7ad7acb8a524dfa830136
Rafael Oliveira <rafael.andre.oliveira@gmail.com>
2022-07-20 18:27:12 -0300

    Adding suport to CIDR masks

--- fortigateobject.py
+++ fortigateobject.py
@@ -3,7 +3,7 @@
 #----------------------------------------------------------------------------
 # Created By  : Rafael Andre de Oliveira (NuxMorpheus)  
 # Creation Date: 11/01/2022
-# version ='1.0'
+# version ='1.1'
 # ---------------------------------------------------------------------------
 # This module generate a script to create Fortigate firewall address 
 # objects, reading the IP adresses from a txt file. 
@@ -28,9 +28,11 @@ else:
 			name=objectname + "_" + line
 			print("edit", name, sep=' ', end='')
 			object=line.rstrip("\n")
-			print("set subnet", object, "255.255.255.255", sep=' ')
+			print("set subnet", object, sep=' ')
 			print("next")
 			count=count+1
 	myfile.close()
 	print ("end")
+
+
 
